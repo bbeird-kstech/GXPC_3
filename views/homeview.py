4f3a793f86356f750975d2c24e2a04aa283f818b
bbeird-kstech <bbeird@kstechservices.com>
2024-07-13 11:33:05 -0400

    Refactored code to reduce confusion. Was creating multiple instances of object unintentionally. This structure is much easier to follow. Equipment Add is working and will populate the client list from the data base. The other dropdowns are stubbed out but there is nothing in the database to retrieve at this point.

--- views/homeview.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-class HomeView(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent, bg="blue")
-        self.label = tk.Label(self, text="Home", font=('Helvetica', 18, 'bold'), fg='white', bg='blue')
-        self.label.pack(expand=True, fill='both')
-        # Debugging: Print statements to check initialization
-        print("HomeView initialized")
--- views/setupview.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-class SetupView(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent, bg="blue")
-        self.label = tk.Label(self, text="Setup", font=('Helvetica', 18, 'bold'), fg='white', bg='blue')
-        self.label.pack(expand=True, fill='both')
-        print("SetupView initialized")
